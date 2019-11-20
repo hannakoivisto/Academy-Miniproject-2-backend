9c74804f6e0a2e5788b4fa13596a2c88879b5120
Meri Kojonen <merikojonen@gmail.com>
2019-11-20 11:11:43 +0200

    .sql -tiedostossa taulujen rakennetta muokattu. Lisätty uudet sarakkeet vastausvaihtoehdoille sekä kysymyskenttä.

--- miniprojekti2.sql
+++ miniprojekti2.sql
@@ -1,27 +1,31 @@
-CREATE TABLE comment (
-    cid serial primary key,
-    username character varying(40) NOT NULL,
-    comment character varying(1000),
-    qid integer
+CREATE TABLE question (
+    id serial primary key,
+    dateadd timestamp NOT NULL,
+    title character varying(400) NOT NULL,
+    details character varying(3000) NOT NULL,
+    optionA character varying(255) NOT NULL,
+    optionB character varying(255) NOT NULL,
+    username character varying(40) NOT NULL
 );
 
-CREATE TABLE question (
-    qid serial primary key,
-    title character varying(255) NOT NULL,
-    username character varying(40) NOT NULL,
-    text character varying(3000)
+CREATE TABLE comment (
+    id serial primary key,
+    dateadd timestamp NOT NULL,
+    comment character varying(1000),
+    username character varying(40),
+    question_id integer
 );
 
 CREATE TABLE vote (
-    vid serial primary key,
-    answer character varying(1000),
-    votecount integer,
-    qid integer
+    id serial primary key,
+    optionAcounter integer,
+    optionBcounter integer,
+    question_id integer
 );
 
 ALTER TABLE comment
-    ADD CONSTRAINT fk_vote_id FOREIGN KEY (qid) REFERENCES question(qid);
+    ADD CONSTRAINT fk_comment_id FOREIGN KEY (question_id) REFERENCES question (id);
 
 ALTER TABLE vote
-    ADD CONSTRAINT fk_vote_id FOREIGN KEY (qid) REFERENCES question(qid);
+    ADD CONSTRAINT fk_vote_id FOREIGN KEY (question_id) REFERENCES question (id);
 
